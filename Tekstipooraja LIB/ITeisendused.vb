2e0a92901242dff8b3687f31291067b470dbe7da
Siim Tišler <sitisl@ttu.ee>
2023-02-08 11:22:32 +0200

    Added interface properties and methods

--- Tekstipooraja LIB/ITeisendused.vb
+++ Tekstipooraja LIB/ITeisendused.vb
@@ -1,3 +1,10 @@
 ﻿Public Interface ITeisendused
+    Property intAlgus As Integer 'Esiemene symbol
+    Property intLopp As Integer  'Viimane symbol
+    Property strTekst As String  'Sisestatud tekst
+
+
+    Function pooraTekst() As String
+    Sub teisendaTekst(ByRef strSisendTekst As String)
 
 End Interface
