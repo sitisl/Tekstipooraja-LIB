9b4edabc83273f904f73088553496f5a0fc63e56
Siim Tišler <sitisl@ttu.ee>
2023-02-08 12:10:46 +0200

    Added forms elements/logic

--- Tekstipooraja konteiner/formKasutajaAken.Designer.vb
+++ Tekstipooraja konteiner/formKasutajaAken.Designer.vb
@@ -22,17 +22,132 @@ Partial Class formKasutajaAken
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.txtSisendtekst = New System.Windows.Forms.TextBox()
+        Me.txtValjundTekst1 = New System.Windows.Forms.TextBox()
+        Me.txtValjundTekst2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnPoora1 = New System.Windows.Forms.Button()
+        Me.btnPoora2 = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(52, 187)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(93, 20)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Sisendtekst"
+        '
+        'txtSisendtekst
+        '
+        Me.txtSisendtekst.Location = New System.Drawing.Point(274, 187)
+        Me.txtSisendtekst.Name = "txtSisendtekst"
+        Me.txtSisendtekst.Size = New System.Drawing.Size(432, 26)
+        Me.txtSisendtekst.TabIndex = 1
+        '
+        'txtValjundTekst1
+        '
+        Me.txtValjundTekst1.Location = New System.Drawing.Point(274, 334)
+        Me.txtValjundTekst1.Name = "txtValjundTekst1"
+        Me.txtValjundTekst1.Size = New System.Drawing.Size(432, 26)
+        Me.txtValjundTekst1.TabIndex = 2
+        '
+        'txtValjundTekst2
+        '
+        Me.txtValjundTekst2.Location = New System.Drawing.Point(274, 521)
+        Me.txtValjundTekst2.Name = "txtValjundTekst2"
+        Me.txtValjundTekst2.Size = New System.Drawing.Size(432, 26)
+        Me.txtValjundTekst2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 334)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(184, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Pööramine funktsiooniga"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(52, 521)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(187, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Pööramine protseduuriga"
+        '
+        'btnPoora1
+        '
+        Me.btnPoora1.Location = New System.Drawing.Point(274, 381)
+        Me.btnPoora1.Name = "btnPoora1"
+        Me.btnPoora1.Size = New System.Drawing.Size(179, 35)
+        Me.btnPoora1.TabIndex = 6
+        Me.btnPoora1.Text = "Pööra funktsiooniga"
+        Me.btnPoora1.UseVisualStyleBackColor = True
+        '
+        'btnPoora2
+        '
+        Me.btnPoora2.Location = New System.Drawing.Point(274, 573)
+        Me.btnPoora2.Name = "btnPoora2"
+        Me.btnPoora2.Size = New System.Drawing.Size(179, 37)
+        Me.btnPoora2.TabIndex = 7
+        Me.btnPoora2.Text = "Pööra protseduuriga"
+        Me.btnPoora2.UseVisualStyleBackColor = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(860, 85)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(80, 35)
+        Me.btnStart.TabIndex = 8
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(1010, 85)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(80, 35)
+        Me.btnStop.TabIndex = 9
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
         '
         'formKasutajaAken
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1147, 772)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnPoora2)
+        Me.Controls.Add(Me.btnPoora1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtValjundTekst2)
+        Me.Controls.Add(Me.txtValjundTekst1)
+        Me.Controls.Add(Me.txtSisendtekst)
+        Me.Controls.Add(Me.label1)
         Me.Name = "formKasutajaAken"
         Me.Text = "Tekstitöötlus"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents label1 As Label
+    Friend WithEvents txtSisendtekst As TextBox
+    Friend WithEvents txtValjundTekst1 As TextBox
+    Friend WithEvents txtValjundTekst2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnPoora1 As Button
+    Friend WithEvents btnPoora2 As Button
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
 End Class
--- Tekstipooraja konteiner/formKasutajaAken.vb
+++ Tekstipooraja konteiner/formKasutajaAken.vb
@@ -2,4 +2,24 @@
     Private Sub formKasutajaAken_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+    Private Sub btnPoora1_Click(sender As Object, e As EventArgs) Handles btnPoora1.Click
+
+        Dim pooraja As Tekstipooraja_LIB.ITeisendused
+
+        pooraja = New Tekstipooraja_LIB.CTekstipooraja
+
+        pooraja.strTekst = txtSisendtekst.Text
+
+        txtValjundTekst1.Text = pooraja.pooraTekst()
+
+    End Sub
+    Private Sub btnPoora2_Click(sender As Object, e As EventArgs) Handles btnPoora2.Click
+        Dim pooraja As Tekstipooraja_LIB.ITeisendused
+        pooraja = New Tekstipooraja_LIB.CTekstipooraja
+
+        pooraja.teisendaTekst(txtSisendtekst.Text)
+        txtValjundTekst2.Text = pooraja.strTekst
+    End Sub
+
+
 End Class
