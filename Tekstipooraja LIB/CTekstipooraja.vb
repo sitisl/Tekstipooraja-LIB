5dbbaf12acdd5658c4f6895e506338cfa96cf67c
Siim Tišler <sitisl@ttu.ee>
2023-02-08 11:48:40 +0200

    Added References

--- Tekstipooraja LIB/CTekstipooraja.vb
+++ Tekstipooraja LIB/CTekstipooraja.vb
@@ -1,3 +1,42 @@
 ﻿Public Class CTekstipooraja
+    Implements ITeisendused
 
+    Private intAlgusSymbol As Integer
+    Private intLoppSymbol As Integer
+    Private strPooratavTekst As String
+
+    Private Property intAlgus As Integer Implements ITeisendused.intAlgus
+        Get
+            Return intAlgusSymbol
+        End Get
+        Set(value As Integer)
+            intAlgusSymbol = value
+        End Set
+    End Property
+
+    Private Property intLopp As Integer Implements ITeisendused.intLopp
+        Get
+            Return intLoppSymbol
+        End Get
+        Set(value As Integer)
+            intLoppSymbol = value
+        End Set
+    End Property
+
+    Private Property strTekst As String Implements ITeisendused.strTekst
+        Get
+            Return strPooratavTekst
+        End Get
+        Set(value As String)
+            strPooratavTekst = value
+        End Set
+    End Property
+
+    Private Sub teisendaTekst(ByRef strSisendTekst As String) Implements ITeisendused.teisendaTekst
+        strPooratavTekst = StrReverse(strSisendTekst)
+    End Sub
+
+    Private Function pooraTekst() As String Implements ITeisendused.pooraTekst
+        Return StrReverse(strPooratavTekst)
+    End Function
 End Class
